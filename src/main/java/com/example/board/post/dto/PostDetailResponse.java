1e8cf3ae1e499363b146b45aeabe9d109b6375ff
Park junseong <20261306@dsm.hs.kr>
2026-09-03 00:27:39 +0900

    feat: create PostDetailResponse

--- src/main/java/com/example/board/post/dto/PostDetailResponse.java
+++ src/main/java/com/example/board/post/dto/PostDetailResponse.java
@@ -1,15 +1,18 @@
 package com.example.board.post.dto;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
 import java.time.LocalDateTime;
 
 @Getter
 @AllArgsConstructor
-public class PostCreateResponse {
+public class PostDetailResponse {
     private Long id;
     private String title;
     private String content;
     private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
 }
